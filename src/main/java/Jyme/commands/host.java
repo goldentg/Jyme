68aef472697ab9a34a6effa043e2a9ebf195d084
GoldenCodex <evan.norman@me.com>
2022-08-23 15:39:43 -0600

    command fixes and improvements

--- src/main/java/Jyme/commands/host.java
+++ src/main/java/Jyme/commands/host.java
@@ -5,11 +5,11 @@ import de.btobastian.sdcf4j.CommandExecutor;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
-import javax.management.MBeanServerConnection;
 import java.awt.*;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
+import java.net.InetAddress;
 import java.text.NumberFormat;
 
 public class host implements CommandExecutor {
@@ -32,17 +32,19 @@ public class host implements CommandExecutor {
 
             double cpuLoad = operatingSystemMXBean.getSystemLoadAverage();
 
+            String hostname = InetAddress.getLocalHost().getHostName();
 
-            message.getChannel().sendMessage(createEmbed(message, format, maxMemory, allocatedMemory, freeMemory, cpuLoad));
+
+            message.getChannel().sendMessage(createEmbed(message, format, maxMemory, allocatedMemory, freeMemory, cpuLoad, hostname));
         } else {
             message.getChannel().sendMessage("You do not have permissions to do this");
         }
     }
 
-    private EmbedBuilder createEmbed(MessageCreateEvent message, NumberFormat format, long maxMemory, long allocatedMemory, long freeMemory, double cpuLoad) {
+    private EmbedBuilder createEmbed(MessageCreateEvent message, NumberFormat format, long maxMemory, long allocatedMemory, long freeMemory, double cpuLoad, String hostname) {
         return new EmbedBuilder()
-                .setTitle("Host Statistics")
-                .setDescription("Free memory: " + format.format(freeMemory / 1024) + "\nAllocated memory: " + format.format(allocatedMemory / 1024) + "\nMax memory: " + format.format(maxMemory / 1024) + "\nTotal free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "\nPercentage free: " + ((double)freeMemory / (double) maxMemory)*100 + "%" + "\nPercent Used: " + ((double)(maxMemory - freeMemory) / (double)maxMemory)*100 + "%" + "\nCPU load: " + cpuLoad)
+                .setTitle("Host Statistics\nHostname: " + hostname)
+                .setDescription("Free memory: " + format.format(freeMemory) + "\nAllocated memory: " + format.format(allocatedMemory) + "\nMax memory: " + format.format(maxMemory) + "\nTotal free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory))) + "\nPercentage free: " + ((double)freeMemory / (double) maxMemory)*100 + "%" + "\nPercent Used: " + ((double)(maxMemory - freeMemory) / (double)maxMemory)*100 + "%" + "\nCPU load: " + cpuLoad)
                 .setColor(new Color(11, 199, 193))
                 .setFooter(message.getMessageAuthor().getDiscriminatedName(), message.getMessageAuthor().getAvatar());
     }
--- src/main/java/Jyme/commands/servers.java
+++ src/main/java/Jyme/commands/servers.java
@@ -3,8 +3,11 @@ package Jyme.commands;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.awt.*;
 
 public class servers implements CommandExecutor {
     @Command(aliases = "servers", description = "Displays server info (bot owner only)")
@@ -14,13 +17,20 @@ public class servers implements CommandExecutor {
             int users = 0;
             int serverc = 0;
             for (Server s : api.getServers()) {
-                servers += "/n" + s.getName() + "(" + s.getMemberCount() + ")";
+                servers += "\n" + s.getName() + "(" + s.getMemberCount() + ")";
                 users += s.getMemberCount();
                 serverc++;
             }
-            message.getChannel().sendMessage("```x1" +"\nServers and member counts (" + serverc + "):" + servers + "\nAverage users Per Server: " + (users / serverc) + "\nTotal Users: " + users + "\nTotal Servers: " + serverc + "\n```");
+            //message.getChannel().sendMessage("```x1" +"\nServers and member counts (" + serverc + "):" + servers + "\nAverage users Per Server: " + (users / serverc) + "\nTotal Users: " + users + "\nTotal Servers: " + serverc + "\n```");
+            message.getChannel().sendMessage(createEmbed(serverc, servers, users));
         } else {
             message.getChannel().sendMessage("You dont have permissions to do this");
         }
+    }
+    private EmbedBuilder createEmbed(int serverc, String servers, int users) {
+        return new EmbedBuilder()
+                .setTitle("Servers and Member Counts")
+                .setDescription("(" + serverc + "):" + servers + "\nAverage users per server: " + (users / serverc) + "\nTotal Users: " + users + "\n Total Servers: " + serverc)
+                .setColor(new Color(11, 199, 193));
     }
 }
